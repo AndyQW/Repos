96e16ac8d23299c04099982b259b33ce23a684ac
Andy Wang <andy.qiang.wang@gmail.com>
2019-04-22 16:39:44 -0400

    Update sqlalchemy_validation.py

--- sqlalchemy_validation.py
+++ sqlalchemy_validation.py
@@ -37,3 +37,16 @@ class User(db.Model):
         raise AssertionError('Provided email is not an email address')
 
       return email
+
+    def set_password(self, password):
+      if not password:
+          raise AssertionError('Password not provided')
+
+      if not re.match('\d.*[A-Z]|[A-Z].*\d', password):
+          raise AssertionError('Password must contain 1 capital letter and 1 number')
+
+      if len(password) < 8 or len(password) > 50:
+          raise AssertionError('Password must be between 8 and 50 characters')
+
+      self.password_hash = generate_password_hash(password)
+
